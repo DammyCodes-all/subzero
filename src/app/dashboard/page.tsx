4c56d4f245baf9378bea452e3e4a34ab63ee7ad1
Dammycodes-all <harliarmeen@gmail.com>
2026-09-12 13:41:46 +0100

    feat: remove unused SubscriptionDetailPage and SubscriptionsLayout components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import { DashboardView } from "@/components/dashboard/DashboardView";
 
-export const instant = false;
-
 export default function DashboardPage() {
   return <DashboardView />;
 }
--- src/app/subscriptions/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SubscriptionDetailView } from "@/components/subscriptions/SubscriptionDetailView";
-
-export default function SubscriptionDetailPage() {
-  return <SubscriptionDetailView />;
-}
